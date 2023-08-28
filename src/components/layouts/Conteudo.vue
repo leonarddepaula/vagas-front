066a1b07ef36d8aa274cf4855ca8b481ff252321
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-08-28 18:22:19 -0300

    scoped and module

---
+++ src/components/layouts/Conteudo.vue
@@ -0,0 +1,34 @@
+<template>
+  <h1>Componente Conteúdo</h1>
+  <p>Propriedade Style: {{ $style }}</p>
+  <div :class="$style['teste-1']">class teste-1</div>
+  <div :id="$style['teste-2']">ID teste-2</div>
+  <home></home>
+  <publicar-vaga></publicar-vaga>
+</template>
+
+<script>
+import PublicarVaga from '../views/PublicarVaga.vue';
+import Home from '../views/Home.vue'
+
+export default {
+    name: 'Conteudo',
+    components: {
+      PublicarVaga,
+      Home
+    }
+}
+</script>
+
+<style module>
+
+
+.teste-1{
+  background-color: red;
+}
+
+#teste-2 {
+  background-color: yellow;
+}
+
+</style>
---
+++ src/components/views/PublicarVaga.vue
@@ -0,0 +1,13 @@
+<template>
+  <p> Publicar Vaga</p>
+</template>
+
+<script>
+export default {
+    name: 'PublicarVaga'
+}
+</script>
+
+<style>
+
+</style>
