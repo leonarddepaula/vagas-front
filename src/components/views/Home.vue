1ce913cdce10d945841d6eb0e930d2b2385a2477
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-08-30 18:43:15 -0300

    personalizando componente home

--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -1,5 +1,52 @@
 <template>
-  <h1>Componente Home</h1>
+  <div class="container py-4">
+    <div class="row">
+      <div class="col">
+        <div class="h-100 p-5 bg-light border rounded-3">
+
+          <h2>Pesquisar vagas</h2>
+          <div class="row">
+            <div class="col">
+              <div class="form-group">
+                <label>Título da vaga</label>
+                <input type="text" class="form-control" placeholder="pesquise por palavras chaves, por exemplo 'PHP', 'Pleno', 'Analista' ">
+                <smal class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</smal>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col">
+              <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+
+    <div class="row mt-5">
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Vagas Abertas</p>
+          <h2>25</h2>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
+          <p>Profisionais cadastrados</p>
+          <h2>125</h2>
+        </div>
+      </div>
+
+      <div class="col-4">
+        <div class="h-100 p-5 rounded-3 border bg-light text-dark">
+          <p>Visitantes online</p>
+          <h2>15</h2>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
