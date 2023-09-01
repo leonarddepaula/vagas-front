c63270fdbb6c9a2290b53aba5cf01165fc357c08
Leonardo De Paula <92495561+leonarddepaula@users.noreply.github.com>
2023-09-01 19:52:02 -0300

    props e components

---
+++ src/components/comuns/PesquisarVagas.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="h-100 p-5 bg-light border rounded-3">
+    <h2>Pesquisar vagas</h2>
+    <div class="row">
+      <div class="col">
+        <div class="form-group">
+          <label>Título da vaga</label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="pesquise por palavras chaves, por exemplo 'PHP', 'Pleno', 'Analista' "
+          />
+          <smal class="form-text text-muted"
+            >Informe palavras que estejam relacionadas com o título da vaga que
+            você procura</smal
+          >
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+    export default {
+        name: 'PesquisaVagas'
+
+    }
+</script>
--- src/components/views/Home.vue
+++ src/components/views/Home.vue
@@ -2,56 +2,40 @@
   <div class="container py-4">
     <div class="row">
       <div class="col">
-        <div class="h-100 p-5 bg-light border rounded-3">
-
-          <h2>Pesquisar vagas</h2>
-          <div class="row">
-            <div class="col">
-              <div class="form-group">
-                <label>Título da vaga</label>
-                <input type="text" class="form-control" placeholder="pesquise por palavras chaves, por exemplo 'PHP', 'Pleno', 'Analista' ">
-                <smal class="form-text text-muted">Informe palavras que estejam relacionadas com o título da vaga que você procura</smal>
-              </div>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <button class="btn btn-outline-dark mt-2" type="button">Buscar</button>
-            </div>
-          </div>
-        </div>
+        <pesquisar-vagas> </pesquisar-vagas>
 
       </div>
     </div>
 
     <div class="row mt-5">
       <div class="col-4">
-        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
-          <p>Vagas Abertas</p>
-          <h2>25</h2>
-        </div>
+        <Indicador titulo="Vagas Abertas" indicador="100"></Indicador>
       </div>
 
       <div class="col-4">
-        <div class="h-100 p-5 rounded-3 border bg-dark text-white">
-          <p>Profisionais cadastrados</p>
-          <h2>125</h2>
-        </div>
-      </div>
+        <indicador titulo="Profissionais Cadastrados" indicador="225"></indicador>
+              </div>
 
       <div class="col-4">
-        <div class="h-100 p-5 rounded-3 border bg-light text-dark">
-          <p>Visitantes online</p>
-          <h2>15</h2>
-        </div>
+        <indicador titulo="Visitantes online" indicador="25"></indicador>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+import PesquisarVagas from '@/components/comuns/PesquisarVagas.vue';
+import Indicador from '@/components/comuns/Indicador.vue'
+
+
+
 export default {
     name: 'Home',
+    components: {
+      PesquisarVagas,
+      Indicador
+      
+    },
     activated(){
       console.log('Componente ativado');
     },
